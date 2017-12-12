22cb040dfb58fa2555e76e40c1fe68c530bf0d1d
jw03 <james.wright@assertis.co.uk>
2017-12-12 10:56:51 -0500

    change grid layout to use flexbox and update hero

--- knob-creek/src/components/hero.js
+++ knob-creek/src/components/hero.js
@@ -4,10 +4,10 @@ class Hero extends React.Component {
   render() {
     return (
       <div className="hero-component">
-      	<div class="hero-bg">
-	        <div class="container row">
-	            <div class="half column hero-text">
-	                <h1 class="header--h1">Maple Never Tasted So Good</h1>
+      	<div className="hero-bg">
+	        <div className="container row flex-grid">
+	            <div className="col-half hero-text">
+	                <h1 className="header--h1">Maple Never Tasted So Good</h1>
 	                <p>We blend this bourbon with natural smoked maple flavors for a unique, smoky sweetness. Full-bodied, inviting maple notes that lift to smoke and are complemented with rich vanilla and caramel. Smoked hickory and maple wood, with hints of earthy grains.
 	                </p>
 	            </div>
