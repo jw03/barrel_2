5db75dda6ece1a9508b756b586f8abe7510bcb62
jw03 <james.wright@assertis.co.uk>
2018-08-23 14:16:41 -0400

    add articles

--- knob-creek/src/components/article.js
+++ knob-creek/src/components/article.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ArticleImage from '../images/www.knobcreek.com-1550074812575410.jpg';
+
 
 class Article extends React.Component {
   render() {
@@ -7,7 +9,54 @@ class Article extends React.Component {
         <h2 className="text-center">Recent Articles</h2>
         <div className="container">
           <div className="article-grid">
-            <div className="col-thirds column article-spacer">
+          <div className="col-thirds article-spacer">
+              <article className="article-item">
+                <a className="article-item--wrapper" href="#">
+
+                  <img class="img-correct" src="/static/media/www.knobcreek.com-1550031134660845.f8f1efcd.jpg" height="42" width="42" />
+
+                  <div className="article-item--description">
+                    <div className="icon-container">
+                    </div>
+                    <p className="date">November 20</p>
+                    <h2>This Creek Runs Full, Making Every Glass Count</h2>
+                    <p className="cta">Read More</p>
+                  </div>
+
+                </a>
+              </article>
+            </div>
+            <div className="col-thirds article-spacer">
+              <article className="article-item">
+                <a className="article-item--wrapper" href="#">
+                  <div className="article-item--image article-item--image--1"></div>
+                  <div className="article-item--description">
+                    <div className="icon-container">
+
+                    </div>
+                    <p className="date">November 20</p>
+                    <h2>This Creek Runs Full, Making Every Glass Count</h2>
+                    <p className="cta">Read More</p>
+                  </div>
+                </a>
+              </article>
+            </div>
+            <div className="col-thirds article-spacer">
+              <article className="article-item">
+                <a className="article-item--wrapper" href="#">
+                  <div className="article-item--image article-item--image--1"></div>
+                  <div className="article-item--description">
+                    <div className="icon-container">
+
+                    </div>
+                    <p className="date">November 20</p>
+                    <h2>This Creek Runs Full, Making Every Glass Count</h2>
+                    <p className="cta">Read More</p>
+                  </div>
+                </a>
+              </article>
+            </div>
+            {/*<div className="col-thirds article-spacer">
               <article className="article-item">
                 <a className="article-item--wrapper" href="#">
                   <div className="article-item--image article-item--image--1"></div>
@@ -23,7 +72,7 @@ class Article extends React.Component {
               </article>
             </div>
 
-            <div className="col-thirds column article-spacer">
+            <div className="col-thirds article-spacer">
               <article className="article-item">
                 <a className="article-item--wrapper" href="#">
                   <div className="article-item--image article-item--image--2">
@@ -40,7 +89,7 @@ class Article extends React.Component {
               </article>
             </div>
 
-            <div className="col-thirds column article-spacer">
+            <div className="col-thirds article-spacer">
               <article className="article-item">
                 <a className="article-item--wrapper" href="#">
                   <div className="article-item--image article-item--image--3">
@@ -57,7 +106,7 @@ class Article extends React.Component {
               </article>
             </div>
 
-            <div className="col-thirds column article-spacer">
+            <div className="col-thirds article-spacer">
               <article className="article-item">
                 <a className="article-item--wrapper" href="#">
                   <div className="article-item--image article-item--image--1"></div>
@@ -73,7 +122,7 @@ class Article extends React.Component {
               </article>
             </div>
 
-            <div className="col-thirds column article-spacer">
+            <div className="col-thirds article-spacer">
               <article className="article-item">
                 <a className="article-item--wrapper" href="#">
                   <div className="article-item--image article-item--image--1"></div>
@@ -88,6 +137,22 @@ class Article extends React.Component {
                 </a>
               </article>
             </div>
+
+            <div className="col-thirds article-spacer">
+              <article className="article-item">
+                <a className="article-item--wrapper" href="#">
+                  <img src={ArticleImage} />
+                  <div className="article-item--description">
+                    <div className="icon-container">
+
+                    </div>
+                    <p className="date">November 20</p>
+                    <h2>This Creek Runs Full, Making Every Glass Count</h2>
+                    <p className="cta">Read More</p>
+                  </div>
+                </a>
+              </article>
+            </div>*/}
           </div>
         </div>
     </div>
