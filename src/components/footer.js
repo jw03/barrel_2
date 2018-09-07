b7fd9fb3ff5183355ae9a2c9596ef50dd241f179
jw03 <james.wright@assertis.co.uk>
2018-09-07 09:56:53 -0400

    fix article layouts - heading - add basic modal

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import BrandLogo from '../images/logos/www.knobcreek.com-1311011787501770.svg';
 
 class Footer extends React.Component {
   render() {
     return (
       <div className="footer-component">
-        Please edit src/components///FooterComponent.js to update this component!
+        <footer>
+	        <nav className="navbar navbar__footer">
+	            <div className="nav-items--social">
+                  <h2 className="navbar-social-share">share on</h2>
+	                <ul className="navbar-social">
+		                <li>
+		                  <a href="http://www.facebook.com" rel="noopener noreferrer" target="_blank">
+		                    <i className="fa fa-facebook"></i>
+		                  </a>
+		                </li>
+		                <li>
+		                  <a href="http://www.twitter.com" rel="noopener noreferrer" target="_blank">
+		                    <i className="fa fa-twitter"></i>
+		                  </a>
+		                </li>
+		              </ul>
+	            </div>
+	            <a className="navbar-item--brand" href="#">
+	                <img className="brand-logo" src={BrandLogo} />
+	            </a>
+	        </nav>
+	    </footer>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,38 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+window.onload=function(){
+// Get modal element
+var modal = document.getElementById('simpleModal');
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target == modal){
+    modal.style.display = 'none';
+  }
+}
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
